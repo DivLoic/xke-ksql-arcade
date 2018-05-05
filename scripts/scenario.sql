ba0d87db0a2f9c1cb5c0e6a6b4e4b8acbdbdd673
Loïc <ldivad@isep.fr>
2018-05-05 19:43:28 +0200

    add the talk script

---
+++ scripts/scenario.sql
@@ -0,0 +1,48 @@
+SHOW TOPICS;
+SHOW QUERIES;
+PRINT 'CLICK-SCREEN';
+
+-- kafka-console-consumer --bootstrap-server localhost:9092 --topic SELECT-SCREEN
+-- watch -t -n 0.5 'kafka-consumer-groups  --list --bootstrap-server localhost:9092 2> /dev/null'
+
+CREATE TABLE FIGHTERS (id STRING , name STRING) WITH ( KAFKA_TOPIC = 'CHARACTERS-REF', VALUE_FORMAT = 'JSON', KEY = 'id');
+
+CREATE STREAM PLAYERS (ts LONG, character STRING, game STRING, human BOOLEAN, player INT) WITH ( KAFKA_TOPIC = 'CLICK-SCREEN', VALUE_FORMAT = 'JSON', TIMESTAMP = 'ts');
+
+SHOW TABLES;
+SHOW STREAMS;
+SHOW QUERIES;
+
+DESCRIBE PLAYERS;
+DESCRIBE EXTENDED PLAYERS;
+
+SHOW STREAMS;
+
+
+SELECT game, human, character FROM PLAYERS WHERE human = true AND game = 'StreetFighter';
+
+SELECT game, human, character, name FROM PLAYERS LEFT JOIN FIGHTERS ON character = id WHERE human = true AND game = 'StreetFighter';
+
+CREATE STREAM HUMANPLAYERS AS SELECT game, human, character, name FROM PLAYERS LEFT JOIN FIGHTERS ON character = id WHERE human = true AND game = 'StreetFighter';
+
+SHOW STREAMS;
+SHOW QUERIES;
+
+SHOW PROPERTIES;
+
+DESCRIBE EXTENDED HUMANPLAYERS;
+
+SET 'commit.interval.ms' = '5000';
+
+CREATE TABLE PLAYERSCHOICE AS SELECT name, count(*) AS SCORE FROM HUMANPLAYERS WINDOW TUMBLING (size 5 seconds) GROUP BY name;
+
+SHOW TABLES;
+SHOW QUERIES;
+
+SELECT * FROM PLAYERSCHOICE;
+
+DESCRIBE EXTENDED PLAYERSCHOICE;
+
+SELECT TIMESTAMPTOSTRING(ROWTIME, 'yyyy-MM-dd HH:mm:ss'), score, name FROM PLAYERSCHOICE;
+
+-- EXPLAIN CTAS_PLAYERSCHOICE;
